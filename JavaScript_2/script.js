a608e4ed8c168fe47c19c9b953a9ec2c5a50ed2a
Angel203 <angelb203@icloud.com>
2024-09-03 18:42:12 -0400

    Working on JavaScript 2 completed 2 requirments

---
+++ JavaScript_2/script.js
@@ -0,0 +1,14 @@
+let myTurtles = ["Box Turtle", "Softshell Turtles", "Freshewater Turtle", "Land Turtle"];
+console.log(myTurtles[3]);
+let myTurtle = 4
+
+const myFavorite = 7;
+
+if (myTurtle < myFavorite){
+    console.log("buy One ;-)!");
+}else if(myTurtle === myFavorite){
+    console.log("not for sale");
+}else{
+    console.log("sold out");
+}
+  
