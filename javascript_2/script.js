596ff9da7364349c32c9b2eadf26b93536cd6c51
Angel203 <angelb203@icloud.com>
2024-09-05 14:47:53 -0400

    completed javascript2

--- javascript_2/script.js
+++ javascript_2/script.js
@@ -11,4 +11,83 @@ if (myTurtle < myFavorite){
 }else{
     console.log("sold out");
 }
-  
+
+document.getElementById("myHeader").innerText = "Welcome to my Turtle Table";
+
+document.getElementsByClassName("content")[0].style.color = "green";
+
+let imgTag = document.getElementsByTagName("img")[0];
+imgTag.setAttribute("turtle", "turtle.jpeg")
+
+let newAttribute = document.createAttribute("src");
+newAttribute.value = "turtle.jpeg";
+imgTag.setAttributeNode(newAttribute);
+
+document.getElementById("font").style.fontSize = "40px";
+
+document.getElementById("myHeader").onclick = function() {
+    alert("Header clicked")
+}
+
+document.getElementById("typesOfTurtles").addEventListener('click',() =>{
+   this.innerText = "Clicked" 
+});
+
+let specialP = document.getElementById("font");
+specialP.onclick = function(){
+    specialP.innerHTML = "Hi I changed so can You!";
+}
+
+document.getElementById("font").onmouseover = function(){
+    this.style.backgroundColor = "lightpink";
+}
+
+window.onload = function(){
+
+}
+
+function welcomePerson(humanName){
+    console.log("Hello, " + humanName + "how are you?");
+}
+welcomePerson("Angel");
+
+function calculateApples(numOne, numTwo, numThree){
+    return numOne + numTwo + numThree;
+}
+console.log(calculateApples(3, 4, 2));
+
+let boxTurtles = 0;
+while (boxTurtles < 6 ) {
+    console.log("My Box Turtle is at" + boxTurtles)
+    boxTurtles++;
+};
+
+for (let index = 0; index < 6; index++){
+    console.log("Looping my Box turtles to: " + index );
+}
+
+const turtleFacts = [
+    "Turtles are the most interesting creaters in the world",
+    "Turtles are reptiles",
+    "Turtles have existed for around 215 million years.",
+    "Sea turtles can hod their breath for up to 5 hours.",
+    "There are 356 species of turtles"
+];
+
+let factIndex = 0;
+let loopCount = 0;
+const maxLoops = 3;
+function displayNextFact() {
+   document.getElementById('turtleFactDisplay').innerText = turtleFacts[factIndex];
+   factIndex = (factIndex + 1) % turtleFacts.length; 
+
+   if (factIndex === 0){
+    loopCount++;
+   }
+    if(loopCount === maxLoops){
+        clearInterval(intervalId);
+        console.log("Stopped displaying turtle facts after 3 loops.");
+    }
+};
+
+const intervalId = setInterval(displayNextFact, 5000);
